931be2522567eeac1710f93af7cf658593cbd7c2
Kumar Vishnu <kumarvishnu@logiq.ai>
2023-01-31 17:44:28 +0000

    GitBook: [#863] No subject

--- .gitbook/assets/logiq-ossec-installer.ps1
+++ .gitbook/assets/logiq-ossec-installer.ps1
@@ -9,9 +9,21 @@ param(
 Invoke-WebRequest 'https://packages.wazuh.com/4.x/windows/wazuh-agent-4.3.10-1.msi' -OutFile logiq-ossec-agent.msi
 
 $hostname = hostname
-.\logiq-ossec-agent.msi /q WAZUH_MANAGER=$LOGIQ_OSSEC_MANAGER WAZUH_REGISTRATION_PASSWORD=$WAZUH_REGISTRATION_PASSWORD WAZUH_AGENT_NAME=$hostname
+.\logiq-ossec-agent.msi /q WAZUH_MANAGER=$LOGIQ_OSSEC_MANAGER WAZUH_REGISTRATION_PASSWORD=$LOGIQ_OSSEC_PASSWORD WAZUH_AGENT_NAME=$hostname
 
 Start-Sleep -Seconds 20
 
 $serviceName = (Get-Service | Where-Object {$_.Name -like "Wazu*"}).Name
 Start-Service $serviceName -PassThru
+
+$hostname = hostname
+$macAddress = (Get-WmiObject -Class Win32_NetworkAdapter -Filter "NetEnabled='True'").MACAddress
+$macAddress = $macAddress -replace ":", ""
+$agentName = "$hostname-$macAddress"
+echo "Agentname: $agentName"
+
+cd 'C:\Program Files (x86)\ossec-agent\'
+
+.\agent-auth.exe -m $LOGIQ_OSSEC_MANAGER -p 1515 -P $LOGIQ_OSSEC_PASSWORD -A $agentName
+
+Restart-Service $serviceName -PassThru
